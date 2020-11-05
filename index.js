8039076d53c0d8d3f6f874181f86f662e90aac96
Jared Strohschein <jared.strohschein@gmail.com>
2020-11-04 22:28:04 -0500

    ..

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+require('dotenv').config()
 const server = require("./api/server.js");
 
-const port = 5000;
+//we need this bc Heroku will tell our app what port to listen on
+//install dotenv
+const port = process.env.PORT;
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
